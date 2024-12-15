d1227664dd579c07fbc2d1919d354cad9dcfa447
Jonathan Monroy <joniosva@gmail.com>
2024-12-15 03:17:03 -0600

    terminado

---
+++ client/src/components/Prices.js
@@ -0,0 +1,32 @@
+export const Prices = [
+  {
+    _id: 0,
+    name: "$0 a 19",
+    array: [0, 19],
+  },
+  {
+    _id: 1,
+    name: "$20 a 39",
+    array: [20, 39],
+  },
+  {
+    _id: 2,
+    name: "$40 a 59",
+    array: [40, 59],
+  },
+  {
+    _id: 3,
+    name: "$60 a 79",
+    array: [60, 79],
+  },
+  {
+    _id: 4,
+    name: "$80 a 99",
+    array: [80, 99],
+  },
+  {
+    _id: 4,
+    name: "$100 a mas",
+    array: [100, 9999],
+  },
+];
---
+++ client/src/pages/CartPage.js
@@ -0,0 +1,207 @@
+import React, { useState, useEffect } from "react";
+import Layout from "./../components/Layout/Layout";
+import { useCart } from "../context/cart";
+import { useAuth } from "../context/auth";
+import { useNavigate } from "react-router-dom";
+import DropIn from "braintree-web-drop-in-react";
+import { AiFillWarning } from "react-icons/ai";
+import axios from "axios";
+import toast from "react-hot-toast";
+import "../styles/CartStyles.css";
+
+const CartPage = () => {
+  const [auth, setAuth] = useAuth();
+  const [cart, setCart] = useCart();
+  const [clientToken, setClientToken] = useState("");
+  const [instance, setInstance] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  //total price
+  const totalPrice = () => {
+    try {
+      let total = 0;
+      cart?.map((item) => {
+        total = total + item.price;
+      });
+      return total.toLocaleString("en-US", {
+        style: "currency",
+        currency: "USD",
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  //detele item
+  const removeCartItem = (pid) => {
+    try {
+      let myCart = [...cart];
+      let index = myCart.findIndex((item) => item._id === pid);
+      myCart.splice(index, 1);
+      setCart(myCart);
+      localStorage.setItem("cart", JSON.stringify(myCart));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  //get payment gateway token
+  const getToken = async () => {
+    try {
+      const data  = {
+          clientToken: 'dsajdajisdiqwndq'
+      }
+      setClientToken(data?.clientToken);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getToken();
+  }, [auth?.token]);
+
+  //handle payments
+  const handlePayment = async () => {
+    try {
+     console.log("pago");
+     
+      setLoading(true);
+
+      const { data } = await axios.post("/api/v1/product/braintree/payment", {
+        cart,
+      });
+      setLoading(false);
+      localStorage.removeItem("cart");
+      setCart([]);
+      navigate("/dashboard/user/orders");
+      toast.success("Payment Completed Successfully ");
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
+  return (
+    <Layout>
+      <div className=" cart-page">
+        <div className="row">
+          <div className="col-md-12">
+            <h1 className="text-center bg-light p-2 mb-1">
+              {!auth?.user
+                ? "Hola invitado"
+                : `Hola  ${auth?.token && auth?.user?.name}`}
+              <p className="text-center">
+                {cart?.length
+                  ? `Tienes ${cart.length} productos en tu carrito ${
+                      auth?.token ? "" : "Porfavor logeate para pagar !"
+                    }`
+                  : " Tu carrito esta vacio"}
+              </p>
+            </h1>
+          </div>
+        </div>
+        <div className="container ">
+          <div className="row ">
+            <div className="col-md-7  p-0 m-0">
+              {cart?.map((p) => (
+                <div className="row card flex-row" key={p._id}>
+                  <div className="col-md-4">
+                    <img
+                      src={`/api/v1/product/product-photo/${p._id}`}
+                      className="card-img-top"
+                      alt={p.name}
+                      width="100%"
+                      height={"130px"}
+                    />
+                  </div>
+                  <div className="col-md-4">
+                    <p>{p.name}</p>
+                    <p>{p.description.substring(0, 30)}</p>
+                    <p>Precio : {p.price}</p>
+                  </div>
+                  <div className="col-md-4 cart-remove-btn">
+                    <button
+                      className="btn btn-danger"
+                      onClick={() => removeCartItem(p._id)}
+                    >
+                      Eliminar
+                    </button>
+                  </div>
+                </div>
+              ))}
+            </div>
+            <div className="col-md-5 cart-summary ">
+              <h2>Carrito</h2>
+
+              <hr />
+              <h4>Total : {totalPrice()} </h4>
+              {auth?.user?.address ? (
+                <>
+                  <div className="mb-3">
+                    <h4>Direccion actual</h4>
+                    <h5>{auth?.user?.address}</h5>
+                    <button
+                      className="btn btn-outline-warning"
+                      onClick={() => navigate("/dashboard/user/profile")}
+                    >
+                      Actualizar direcccion
+                    </button>
+                  </div>
+                </>
+              ) : (
+                <div className="mb-3">
+                  {auth?.token ? (
+                    <button
+                      className="btn btn-outline-warning"
+                      onClick={() => navigate("/dashboard/user/profile")}
+                    >
+                      Actualizar direcccion
+                    </button>
+                  ) : (
+                    <button
+                      className="btn btn-outline-warning"
+                      onClick={() =>
+                        navigate("/login", {
+                          state: "/cart",
+                        })
+                      }
+                    >
+                      Porfavor inicia sesion para pagar
+                    </button>
+                  )}
+                </div>
+              )}
+              <div className="mt-2">
+                {!auth?.token || !cart?.length ? (
+                  ""
+                ) : (
+                  <>
+                    <DropIn
+                      options={{
+                        authorization: clientToken,
+                        paypal: {
+                          flow: "vault",
+                        },
+                      }}
+                      onInstance={(instance) => setInstance(instance)}
+                    />
+
+                    <button
+                      className="btn btn-primary"
+                      onClick={handlePayment}
+                      disabled={!auth?.user?.address}
+                    >
+                      {loading ? "Procesando ...." : "Pagar"}
+                    </button>
+                  </>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default CartPage;
